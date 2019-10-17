639b0c6329f20578aa06c8330a8dfb1dff87809f
bill <wherhard@student.rtc.edu>
2019-10-16 22:22:25 -0700

    Downloads new repo

--- GithubCheck.py
+++ GithubCheck.py
@@ -2,6 +2,14 @@
 import requests
 
 
+def download_repo():
+    repo_endpoint = 'https://api.github.com/repos/%s/%s/%s/' % ("billerhard","CI-CD-Flaskr-Pipeline", 'zipball')
+    args={}
+    response = requests.get(repo_endpoint,params=args)
+    with open('./repo.zip', "wb") as f:
+        f.write(response.content)
+
+
 def main():
     # do stuff
     github_endpoint = "https://api.github.com/repos/%s/%s/commits" % ("billerhard", "CI-CD-Flaskr-Pipeline")
@@ -13,6 +21,7 @@ def main():
         if previous_time<item['commit']['author']['date']:
             print('new commit found!')
             previous_time=item['commit']['author']['date']
+            download_repo()
 
 
 if __name__ == '__main__':
