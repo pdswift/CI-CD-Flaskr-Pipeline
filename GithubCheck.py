6c9d08e7744b104e3b52ce6c5f820afab3411801
bill <wherhard@student.rtc.edu>
2019-10-16 21:58:41 -0700

    Found a new commit!

--- GithubCheck.py
+++ GithubCheck.py
@@ -4,14 +4,15 @@ import requests
 
 def main():
     # do stuff
-    master="957acb944661f8ba6b462381fc719a79b1a6fd3d"
     github_endpoint = "https://api.github.com/repos/%s/%s/commits" % ("billerhard", "CI-CD-Flaskr-Pipeline")
     previous_time="2019-10-17T01:01:01Z"
-    args = {'sha':master, 'since':previous_time}
+    args = {'since':previous_time}
 
     response = requests.get(github_endpoint, params=args)
-    print(response)
-    print(response.json())
+    for item in response.json():
+        if previous_time<item['commit']['author']['date']:
+            print('new commit found!')
+            previous_time=item['commit']['author']['date']
 
 
 if __name__ == '__main__':
